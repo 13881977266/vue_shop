da4a02b9753b8a99a0600357d82a5dc3649e8f17
huhao <1007093507@qq.com>
2020-05-29 17:51:48 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// import global css
+import './assets/css/global.css'
+
+// 导入axios
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8080/api/private/v1/api2'
+// axios.defaults.baseURL = '/api2'
+Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
 
